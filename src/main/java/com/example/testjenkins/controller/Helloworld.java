e2bcb46665cd62f6ba4b57c0fa16a86dbcec5c6f
gengsai <gengsai@lattebank.com>
2021-03-03 14:58:53 +0800

    first commit

---
+++ src/main/java/com/example/testjenkins/controller/Helloworld.java
@@ -0,0 +1,19 @@
+package com.example.testjenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class Helloworld {
+
+    @GetMapping
+    public String getMessage(String req){
+
+        if (req == "s"){
+            System.out.println("hello world");
+        }
+        return "helloworld";
+    }
+}
