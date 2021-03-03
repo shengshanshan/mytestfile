af8ebbb78850b61672b8c2e50ba29d997139521b
gengsai <gengsai@lattebank.com>
2021-03-03 15:04:20 +0800

    测试jenkins

--- src/main/java/com/example/testjenkins/controller/Helloworld.java
+++ src/main/java/com/example/testjenkins/controller/Helloworld.java
@@ -14,6 +14,6 @@ public class Helloworld {
         if (req == "s"){
             System.out.println("hello world");
         }
-        return "helloworld";
+        return "helloworld-test-jenkins";
     }
 }
